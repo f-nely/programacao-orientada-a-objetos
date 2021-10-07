d3bb7f5c0b114c6e15d174d6eb4ca3aaf7c34ade
nely de faria <nelly.nf54@gmail.com>
2021-10-07 17:45:53 -0300

    Criando arquivo Main.java

---
+++ src/com/nelynely/tema1/modulo1/Main.java
@@ -0,0 +1,12 @@
+package com.nelynely.tema1.modulo1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Aluno aluno = new Aluno("Taylor", 34);
+        System.out.println("O nome do aluno é " + aluno.recuperarNome());
+
+        Referencia referencia = new Referencia();
+    }
+}
