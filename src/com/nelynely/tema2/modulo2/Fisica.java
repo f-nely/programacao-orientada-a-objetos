238cc41e99365addf4c2401cadf6ff9592bef5d4
nely de faria <nelly.nf54@gmail.com>
2021-10-09 20:20:36 -0300

    Criando arquivo Fisica.java

---
+++ src/com/nelynely/tema2/modulo2/Fisica.java
@@ -0,0 +1,12 @@
+package com.nelynely.tema2.modulo2;
+
+import java.util.Calendar;
+
+public class Fisica extends Pessoa {
+
+    private String CPF;
+
+    public Fisica(String nome, String CPF, String nacionalidade, String naturalidade) {
+        super(nome, CPF, nacionalidade, naturalidade);
+    }
+}
