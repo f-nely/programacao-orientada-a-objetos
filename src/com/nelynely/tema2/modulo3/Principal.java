40cfc23178984e94cdb5227c4c44ad114153c1b1
nely de faria <nelly.nf54@gmail.com>
2021-10-09 20:56:17 -0300

    Criando arquivo Principal.java

---
+++ src/com/nelynely/tema2/modulo3/Principal.java
@@ -0,0 +1,19 @@
+package com.nelynely.tema2.modulo3;
+
+public class Principal {
+
+    private static Pessoa ref[];
+
+    public static void main(String[] args) {
+
+        ref = new Pessoa[2];
+        //Calendar data_nasc = Calendar.getInstance();
+        //Calendar data_criacao = Calendar.getInstance();
+        //data_nasc.set(1980 , 10 , 23);
+        ref[0] = new Fisica("Marco Antônio", null, "Brasil", "Rio de Janeiro"); //"365.586.875-45"
+        //data_criacao.set(1913 , 02 , 01 );
+        ref[1] = new Juridica("Escola Novo Mundo Ltda", null, "Brasil", "Rio de Janeiro"); //"43.186.666/0026-32"
+        ref[0].atualizarID("365.586.875-45");
+        ref[1].atualizarID("43.186.666/0026-32");
+    }
+}
