b614e5765a68e4400e19b6d78b0b83dcb0572e97
nely de faria <nelly.nf54@gmail.com>
2021-10-10 14:29:43 -0300

    Criando arquivo Chamador.java

---
+++ src/com/nelynely/tema3/modulo1/Chamador.java
@@ -0,0 +1,10 @@
+package com.nelynely.tema3.modulo1;
+
+public class Chamador {
+
+    Arranjo arrj = new Arranjo();
+
+    int invocarGetElemento(int i) {
+        return arrj.getElemento(i);
+    }
+}
