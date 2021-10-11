356f0cc31af62023e7d2961334460d5ed71eb8ce
nely de faria <nelly.nf54@gmail.com>
2021-10-11 16:41:08 -0300

    Criando arquivo ThreadSubclasse.java

---
+++ src/com/nelynely/tema4/modulo1/ThreadSubclasse.java
@@ -0,0 +1,14 @@
+package com.nelynely.tema4.modulo1;
+
+public class ThreadSubclasse extends Thread {
+
+    long numero;
+
+    ThreadSubclasse (long numero) {
+        this.numero = numero;
+    }
+
+    public void run() {
+        // implementa o comportamento apropriado
+    }
+}
