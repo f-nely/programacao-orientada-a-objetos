f62a08873f3221f1332948ff965f3d32bf2fab05
nely de faria <nelly.nf54@gmail.com>
2021-10-11 18:07:10 -0300

    Criando arquivo Principal.java

---
+++ src/com/nelynely/tema4/modulo2/Principal.java
@@ -0,0 +1,10 @@
+package com.nelynely.tema4.modulo2;
+
+public class Principal {
+
+    private static PingPong partida;
+
+    public static void main(String[] args) throws InterruptedException {
+        partida = new PingPong(8);
+    }
+}
