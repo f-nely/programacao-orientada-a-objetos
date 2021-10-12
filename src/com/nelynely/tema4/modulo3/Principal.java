34dff4570e5c8dbe25cb199813ff9e47875bedd2
nely de faria <nelly.nf54@gmail.com>
2021-10-12 11:42:54 -0300

    Criando arquivo Principal.java

---
+++ src/com/nelynely/tema4/modulo3/Principal.java
@@ -0,0 +1,11 @@
+package com.nelynely.tema4.modulo3;
+
+public class Principal {
+
+    private static Empresa ACME;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ACME = new Empresa(20, 25, 4, 200);
+    }
+}
