7cc55370810ad06e6894f2b47764379289478929
nely de faria <nelly.nf54@gmail.com>
2021-10-12 11:39:39 -0300

    Criando arquivo ContadorSinc.java

---
+++ src/com/nelynely/tema4/modulo3/ContadorSinc.java
@@ -0,0 +1,38 @@
+package com.nelynely.tema4.modulo3;
+
+/**
+ *
+ * @author Prof Marlos M Corrêa
+ */
+
+public class ContadorSinc {
+
+    private int contador;
+    private final int inicio;
+
+    public ContadorSinc ( int inicio ) {
+        this.inicio = inicio;
+        this.contador = inicio;
+    }
+    public synchronized void decrementar ( ) {
+        this.contador--;
+    }
+    public synchronized void decrementar ( int n ) {
+        this.contador = this.contador - n;
+    }
+    public synchronized void incrementar ( ) {
+        this.contador++;
+    }
+    public synchronized void incrementar ( int n ) {
+        this.contador = this.contador + n;
+    }
+    public synchronized void resetContador ( ) {
+        this.contador = this.inicio;
+    }
+    public synchronized void zerarContador ( ) {
+        this.contador = 0;
+    }
+    public synchronized int getContador ( ) {
+        return this.contador;
+    }
+}
